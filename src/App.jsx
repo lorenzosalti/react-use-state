56b1fed8c7a3e2dca8de45df1ebe8a2d5ed9c773
lorenzosalti <lorenzosalti@gmail.com>
2025-04-08 18:17:00 +0200

    feat: card with html content

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import ButtonsList from "./components/ButtonsList"
 import Card from "./components/Card"
 
+import languages from "./data/languages"
+
 
 
 function App() {
 
   return (
     <>
-      <ButtonsList />
-      {/* <Card /> */}
+      <h1>Learn Web Development</h1>
+      <ButtonsList data={languages} />
+      <Card data={languages} />
     </>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,12 @@
-function Card() {
+function Card({ data }) {
 
+  return <div>
 
+    <h2>{data[0].title}</h2>
+
+    <p>{data[0].description}</p>
+
+  </div>
 
 }
 
